5320bcc36e267d4472fb32d8b8b1ab6be1c4e92f
Atharva Raj Thakur <69038796+atharva00721@users.noreply.github.com>
2023-03-08 22:05:09 +0530

    added score board

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ def printBoard(value):
     print("\n")  
     print("\t      |      |")  
     print(f"\t  {value[0]}   |  { value[1]}   |  {value[2]}")  
-    print('\t______|______|______')  
+    print('\t______|______|______')
     print("\t      |      |") 
     print(f"\t  {value[3]}   |  {value[4]}   |  {value[5]}")  
-    print('\t______|______|______')  
+    print('\t______|______|______')
     print("\t      |      |")  
     print(f"\t  {value[6]}   |  {value[7]}   |  {value[8]}")  
     print("\t      |      |")  
@@ -60,11 +60,9 @@ def bot(board):
     m = random.choice(availableMoves)
     return m
 
-
-def score():
     
 if __name__ == "__main__":
-    turn = 1 # 1 == X, 0 == O
+    
     print("""
     ----------------------------------
     |                                |   
@@ -73,7 +71,10 @@ if __name__ == "__main__":
     ----------------------------------
     """)
     mode = 1
+    turn = 1 # 1 == X, 0 == O
+    score_y, score_x = 0,0
     while True:
+        
         print("""
         Enter 1 to play
         Enter 2 to quit""")
@@ -94,33 +95,43 @@ if __name__ == "__main__":
                         if checkAvailable(board[move - 1]):
                             board[move - 1] = "X"
                             printBoard(board)
-                            if checkWin(board):
-                                print("X WON")    
-                                break 
+                            winstatus = checkWin(board)
+                            if winstatus == True:
+                                score_x += 1
+                                print("X Won")
+                                break
                             turn -= 1
                             print("O's Chance")
                         else:
                             print("Please select another position")
                     except ValueError:
-                        print("a number between 1-9")
-                    
+                        print("a number between 1-9") 
                 else:
                     bot_move = int(bot(board))
                     # bot_move = 1
                     if checkAvailable(board[bot_move-1]):
                         board[bot_move - 1] = "O"
                         printBoard(board)
-                        if checkWin(board):
-                            print("O WON")    
-                            break   
+                        winstatus = checkWin(board)
+                        if winstatus == True:
+                            score_y += 1
+                            print("O Won")
+                            break                                
                         turn += 1
                         print("X's Chance")
-                
-                
             except ValueError:
                 print("fuck you use a number between 1 to 9")
                 break
             if empty(board):
                 print("Game Over! It's a tie")
                 break
-                    
+        print(f"""
+                ----------------------------------
+                | PLAYER  |   SCORE              |
+                |________________________________|
+                |    X    |     {score_x}                |
+                |________________________________|
+                |    Y    |     {score_y}                |
+                |         |                      |
+                ----------------------------------
+                """)          
